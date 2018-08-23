8024423c351936cccc8c0b5a6546c078d084c3dc
Joshua F. Rountree <joshua.rountree@scripps.com>
2018-08-23 11:05:53 -0400

    Updates for login

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
-gem 'coffee-rails', '~> 4.1.0'
+gem 'coffee-rails', '> 4.1.0'
 
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
@@ -25,10 +25,11 @@ gem "aws-sdk-s3", require: false
 
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
-gem 'cars', :git => 'git://github.com/openpilot-community/cars_abstraction.git'
+gem 'cars', :git => 'https://github.com/openpilot-community/cars_abstraction'
 gem 'rack-cors'
 gem 'hashdiff'
 gem 'amoeba'
+gem 'listen'
 gem 'mechanize'
 gem 'awesome_nested_set'
 gem 'friendly_id', '~> 5.2.0'
@@ -57,7 +58,7 @@ end
 
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
-  gem 'web-console', '~> 2.0'
+  # gem 'web-console', '~> 2.0'
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+
+require 'open-uri'
 # == Schema Information
 #
 # Table name: users
@@ -16,29 +18,28 @@ class User < ApplicationRecord
   has_one_attached :avatar
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :registerable,
-         :recoverable, :rememberable, :validatable
   devise :omniauthable, omniauth_providers: %i[github]
   has_one :login
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      # user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
-      # If you are using confirmable and the provider(s) you use validate emails, 
-      # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
-    end
+    new_user = User.find_or_initialize_by(provider: auth.provider, uid: auth.uid)
+    new_user.email = auth.info.email
+    # new_user.password = Devise.friendly_token[0,20]
+    new_user.name = auth.info.name   # assuming the user model has a name
+    avatar_img_file = open(auth.info.image)
+    new_user.github_username = auth.info.nickname
+    new_user.slack_username = auth.info.nickname
+    new_user.avatar.attach(
+      io: avatar_img_file,
+      filename: auth.info.nickname
+    )
+    new_user.save
+    new_user
   end
-
-  class User < ApplicationRecord
-    def self.new_with_session(params, session)
-      super.tap do |user|
-        if data = session["devise.github_data"] && session["devise.github_data"]["extra"]["raw_info"]
-          user.email = data["email"] if user.email.blank?
-        end
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.github_data"] && session["devise.github_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
       end
     end
   end

f2dc5383d926d3aae362f275c9f81986fff12c74
Joshua F. Rountree <joshua.rountree@scripps.com>
2018-08-27 23:03:54 -0400

    Rescue from failed image scrape

--- app/models/vehicle_config.rb
+++ app/models/vehicle_config.rb
@@ -495,19 +495,23 @@ class VehicleConfig < ApplicationRecord
   end
 
   def scrape_image
-    if !self.image.attached?
-      make_name_parameter = vehicle_make.name.parameterize(separator: '_').downcase
-      model_name_parameter = vehicle_model.name.gsub('-',' ').parameterize(separator: '_').downcase
-      model_parameter = "#{make_name_parameter}-#{model_name_parameter}-#{year}"
-      trim_info = Cars::Vehicle.retrieve("#{model_parameter}/trims")
-      image_url = trim_info[:image]
-      tempfile = Down.download(image_url)
-      
-      self.image.attach(
-        io: tempfile,
-        filename: "#{slug}.#{tempfile.original_filename}",
-        content_type: tempfile.content_type
-      )
+    begin
+      if !self.image.attached?
+        make_name_parameter = vehicle_make.name.parameterize(separator: '_').downcase
+        model_name_parameter = vehicle_model.name.gsub('-',' ').parameterize(separator: '_').downcase
+        model_parameter = "#{make_name_parameter}-#{model_name_parameter}-#{year}"
+        trim_info = Cars::Vehicle.retrieve("#{model_parameter}/trims")
+        image_url = trim_info[:image]
+        tempfile = Down.download(image_url)
+        
+        self.image.attach(
+          io: tempfile,
+          filename: "#{slug}.#{tempfile.original_filename}",
+          content_type: tempfile.content_type
+        )
+      end
+    rescue
+      puts "Failed to scrape image"
     end
   end
     

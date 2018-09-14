cb1f7b0872729c90773b0c0e91570f819b3a807a
Joshua F. Rountree <joshua.rountree@scripps.com>
2018-09-14 17:34:30 -0400

    Removes raven for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :set_raven_context
+  # before_action :set_raven_context
   before_action :authenticate_user!
   before_action :set_paper_trail_whodunnit
   # Prevent CSRF attacks by raising an exception.
@@ -12,10 +12,10 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def set_raven_context
-    if Rails.env.production?
-      Raven.user_context(id: session[:current_user_id]) # or anything else in session
-      Raven.extra_context(params: params.to_unsafe_h, url: request.url)
-    end
-  end
+  # def set_raven_context
+  #   if Rails.env.production?
+  #     Raven.user_context(id: session[:current_user_id]) # or anything else in session
+  #     Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+  #   end
+  # end
 end
